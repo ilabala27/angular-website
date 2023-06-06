d7e9c792305ec6130efc60f0edb5d6537c522d52
ilabala27 <ilabalamurugan@gmail.com>
2023-06-06 11:40:54 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,43 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { FormsModule } from '@angular/forms';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about',component: AboutComponent },
+  { path: 'contact',      component: ContactComponent },
+  { path: 'login',      component: LoginComponent },
+  { path: 'register',      component: RegisterComponent },
+ 
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    RegisterComponent,
+    LoginComponent,
+    AboutComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
