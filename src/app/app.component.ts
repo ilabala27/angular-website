d6e07beab53c37db5aeac39e0ef240caff8f68fd
ilabala27 <ilabalamurugan@gmail.com>
2023-06-11 18:30:02 +0530

    home page changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'new-website';
+  showHeader = true;
+
+  constructor(private router: Router) {
+
+    router.events.subscribe(
+      (val) => {
+        if (val instanceof NavigationEnd) {
+          if (val.url == '/login' || val.url == '/register' ) {
+            this.showHeader = false
+          }
+          else{
+            this.showHeader=true
+          }
+        }
+      }
+    )
+
+
+  }
+
 }
